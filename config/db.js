39079df891967c4b4017a439386b5b1d0a955b25
arpit <sarpit4545@gmail.com>
2024-03-17 00:21:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const config = {
     user: process.env.user,
     password: process.env.password,
-    connectString: '(description= (retry_count=1)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-mumbai-1.oraclecloud.com))(connect_data=(service_name=gd293a7f3373c76_quantumdb_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))',
+    connectString: process.env.db_string,
   };  
 
 async function connect() {
