2defe416eef5165c2326fdbfdc1f16019b647de5
arpit <sarpit4545@gmail.com>
2024-04-05 13:28:00 +0530

    CHANGED TITLE

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'DEV' });
+  res.render('index', { title: 'MAIN' });
 });
 
 module.exports = router;
